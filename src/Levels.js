b5577f4faf52b18bce7701430973499e315fba6d
Scott Livingston <swlivingston88@gmail.com>
2021-02-07 15:32:52 -0600

    Fix - actually 17 levels

--- src/Levels.js
+++ src/Levels.js
@@ -34,7 +34,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : mathService.ops.plus
             }
-        case 7:
+        case 5:
             let m = mathService.ops.multiply;
             m.max = 4;
             return {
@@ -42,7 +42,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : m
             }
-        case 5:
+        case 6:
             return {
                 moveChance : 30,
                 winningScore : 14,
@@ -56,7 +56,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : m1
             }
-        case 6:
+        case 7:
             let r6 = mathService.ops.plus;
             r6.max = 40;
             return {
@@ -64,7 +64,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r6
             }
-        case 7:
+        case 8:
             let r7 = mathService.ops.multiply;
             r7.max = 7;
             return {
@@ -72,7 +72,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r7
             }
-        case 8:
+        case 9:
             let r8 = mathService.ops.minus;
             r8.max = 20;
             return {
@@ -80,7 +80,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r8
             }
-        case 7:
+        case 10:
             let m3 = mathService.ops.multiply;
             m3.max = 8;
             return {
@@ -88,7 +88,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : m3
             }
-        case 9:
+        case 11:
             let r9 = mathService.ops.plus;
             r9.max = 80;
             return {
@@ -96,7 +96,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r9
             }
-        case 10:
+        case 12:
             let r10 = mathService.ops.multiply;
             r10.max = 9;
             return {
@@ -104,7 +104,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r10
             }
-        case 11:
+        case 13:
             let r11 = mathService.ops.minus;
             r11.max = 40;
             return {
@@ -112,7 +112,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r11
             }
-        case 12:
+        case 14:
             let r12 = mathService.ops.plus;
             r12.max = 100;
             return {
@@ -120,7 +120,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r12
             }
-        case 13:
+        case 15:
             let r13 = mathService.ops.multiply;
             r13.max = 10;
             return {
@@ -128,7 +128,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r13
             }
-        case 14:
+        case 16:
             let r14 = mathService.ops.minus;
             r14.max = 100;
             return {
@@ -136,7 +136,7 @@ export function getLevel(level = 0) {
                 winningScore : 14,
                 mathObject : r14
             }
-        case 13:
+        case 17:
             let r15 = mathService.ops.multiply;
             r15.max = 12;
             return {
